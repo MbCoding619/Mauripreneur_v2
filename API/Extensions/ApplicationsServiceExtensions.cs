12a6f4ce4b2712b97618a10382f46fcde1180e30
MbCoding619 <beebeejaun.moossa@gmail.com>
2022-07-06 19:13:42 +0400

    added new connection string

--- API/Extensions/ApplicationsServiceExtensions.cs
+++ API/Extensions/ApplicationsServiceExtensions.cs
@@ -16,32 +16,64 @@ namespace API.Extensions
     public static class ApplicationsServiceExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
-        {   
+        {
 
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             //Services for the Token
             services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IPhotoService,PhotoService>();
-            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IJobRepository, JobRepository>();
-            services.AddScoped<IBidRepository,BidRepository>();
-            services.AddScoped<ISmeRepository,SmeRepository>();
-            services.AddScoped<IProfRepository,ProfRepository>();
-            services.AddScoped<IFieldRepository,FieldRepository>();
-            services.AddScoped<IMeetingRepository,MeetingRepository>();
-            services.AddScoped<ISubFieldRepository,SubFieldRepository>();
-            services.AddScoped<ISkillsRepository,SkillsRepository>();
-            services.AddScoped<IQualRepository,QualiRepository>();
-            services.AddScoped<IExperienceRepository,ExperienceRepository>();
-           
+            services.AddScoped<IBidRepository, BidRepository>();
+            services.AddScoped<ISmeRepository, SmeRepository>();
+            services.AddScoped<IProfRepository, ProfRepository>();
+            services.AddScoped<IFieldRepository, FieldRepository>();
+            services.AddScoped<IMeetingRepository, MeetingRepository>();
+            services.AddScoped<ISubFieldRepository, SubFieldRepository>();
+            services.AddScoped<ISkillsRepository, SkillsRepository>();
+            services.AddScoped<IQualRepository, QualiRepository>();
+            services.AddScoped<IExperienceRepository, ExperienceRepository>();
 
-            
+
+
             //Iinitialising the dependency of Automapper in the App Service class
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-                //See below format and understand.
+            //See below format and understand.
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                // Depending on if in development or production, use either Heroku-provided
+                // connection string, or development connection string from env var.
+                if (env == "Development")
+                {
+                    // Use connection string from file.
+                    connStr = config.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+                }
+
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from Heroku, use it to set up your DbContext.
+                options.UseNpgsql(connStr);
 
             });
 
