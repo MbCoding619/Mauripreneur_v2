3f9ba2d15a8044e5e89f7c92235758b6b910d4c4
MbCoding619 <beebeejaun.moossa@gmail.com>
2022-07-06 20:49:59 +0400

    Fourth Possible Fix

--- API/Program.cs
+++ API/Program.cs
@@ -2,36 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using API.Extensions;
+using API.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            try
-            {   
-                var context = services.GetRequiredService<DataContext>();
-                await context.Database.MigrateAsync();
-
-            }catch(Exception ex)
+            var webHost = CreateHostBuilder(args).Build();
+            using (var scope = webHost.Services.CreateScope())
             {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex,"An error occured during migrations");
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<DataContext>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
             }
 
-            host.Run();
-            
+            webHost.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
